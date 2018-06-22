50234fe1bd89d074bea0e4d3811eddda6b7ed5fe
shaan1337 <sniper111@gmail.com>
2018-06-22 10:00:25 +0400

    Add visualize icon to event view near "Metadata" Show "Visualize" when hoving over eye icon

--- src/js/modules/streams/controllers/StreamsItemEventCtrl.js
+++ src/js/modules/streams/controllers/StreamsItemEventCtrl.js
@@ -5,16 +5,29 @@ define(['./_module'], function (app) {
     return app.controller('StreamsItemEventCtrl', [
 		'$scope', '$state', '$stateParams', 'StreamsService',
 		function ($scope, $state, $stateParams, streamsService) {
-			
+
 			$scope.streamId = $stateParams.streamId;
 			$scope.isMetadata = $state.current.data.metadata;
 			$scope.eventNumber = $scope.isMetadata ? 'metadata' : $stateParams.eventNumber;
+
+			$scope.visualize = function (event) {
+				$state.go('visualize.eventflow', {correlationId: event.metaDataParsed.$correlationId});
+			};
 
 			streamsService.eventContent($scope.streamId, $scope.eventNumber)
 			.success(function (data) {
 				$scope.evt = data;
 				$scope.isNotTheSame = data.positionStreamId !== data.streamId || data.positionEventNumber !== data.eventNumber;
 				$scope.links = data.links;
+
+				if($scope.evt.metaData){
+					try{
+						$scope.evt.metaDataParsed = JSON.parse($scope.evt.metaData);
+					}
+					catch(err){
+						$scope.evt.metaDataParsed = null;
+					}
+				}
 
 				if($scope.isMetadata) {
 					// if this was a metadata, we do not need to update anything
